225605d004b3efef4d502b2a35bf9f30c44856b2
SergioPD <sergiodupe@live.com>
2020-11-11 03:06:22 -0600

    Equipo Persea AvoChallenge

---
+++ src/hackatonavo/HackatonAvo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackatonavo;
+
+/**
+ *
+ * @author Sergio
+ */
+public class HackatonAvo {
+
+    public static void main(String[] args) {
+        Interfaz I= new Interfaz();
+        I.setVisible(true);
+        
+    }
+}
